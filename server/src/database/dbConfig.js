dfd1b41c59ce92ff670d40b9f730e3eadf422e88
MarwanYoussef <Marwanusif1@hotmail.com>
2020-09-15 12:37:07 +0200

    fixing vulnerablities

--- server/src/database/dbConfig.js
+++ server/src/database/dbConfig.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // mongo database URI
-const mongoUri = "mongodb://guestbook:private1@ds123695.mlab.com:23695/guestbook";
+const mongoUri = process.env.mongoUri;
 
 const configObj = {
   useUnifiedTopology: true,
